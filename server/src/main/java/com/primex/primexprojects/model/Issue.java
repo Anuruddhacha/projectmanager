700cba794b260045a8fc4c030d5e9e8976bb52ce
anuruddhacha <e17038@eng.pdn.ac.lk>
2025-07-20 12:54:49 +0530

    models are created

--- server/src/main/java/com/primex/primexprojects/model/Issue.java
+++ server/src/main/java/com/primex/primexprojects/model/Issue.java
@@ -1,8 +1,13 @@
 package com.primex.primexprojects.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -12,8 +17,22 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String title;
+    private String description;
+    private String status;
+    private Long projectId;
+    private String priority;
+    private LocalDate dueData;
+    private List<String> tags = new ArrayList<>();
+
     @ManyToOne
     private User assignee;
 
+    @JsonIgnore
+    @ManyToOne
+    private Project project;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
