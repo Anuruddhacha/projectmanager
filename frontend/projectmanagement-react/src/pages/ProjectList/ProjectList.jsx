aa146587407144a312d9dc9b5e8fbfb17269c023
ac3491 <ac3491@zebra.com>
2025-07-29 14:19:11 +0530

    frontend initiated

---
+++ frontend/projectmanagement-react/src/pages/ProjectList/ProjectList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from '../../components/ui/button'
+
+const ProjectList = () => {
+  return (
+     <div className='relative px-5 justify-center'>
+      <Button></Button>
+    </div>
+  )
+}
+
+export default ProjectList
---
+++ frontend/projectmanagement-react/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProjectList from '../ProjectList/ProjectList'
+import {Button} from '../../components/ui/button'
+
+const Home = () => {
+  return (
+    <div><Button></Button></div>
+  )
+}
+
+export default Home
